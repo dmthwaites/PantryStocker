da4008da1e7fbf3338c7512bf0537c1241f1ff4e
Amelia Lane <amelia12@hotmail.co.uk>
2023-02-15 22:39:54 +0000

    created pantry list

---
+++ PantryStockerHosted/PantryStockerHosted/Server/Controllers/PantryItemsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PantryStockerHosted.Shared.PantryItems;
+
+namespace PantryStockerHosted.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PantryItemsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<PantryItem> Get() 
+        { 
+            var pantry = new List<PantryItem>();
+            pantry.Add(new PantryItem { Name = "Brown Rice", Description = "it's brown", Amount = 500, Measure = "grams" });
+            pantry.Add(new PantryItem { Name = "Spaghetti", Description = "it's long", Amount = 1.5, Measure = "kilos" });
+            pantry.Add(new PantryItem { Name = "Spaghetti", Description = "it's long", Amount = 1.5, Measure = "kilos" });
+            return pantry;
+        }
+    }
+}
---
+++ PantryStockerHosted/PantryStockerHosted/Shared/PantryItems/PantryItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PantryStockerHosted.Shared.PantryItems
+{
+    public class PantryItem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Amount { get; set; }
+        public string Measure { get; set; }
+
+    }
+}
